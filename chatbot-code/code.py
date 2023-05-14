4150d3147db10df8e297c3b2563c37cd665b52c2
Joel Myers <joeldmyers@gmail.com>
2023-05-14 11:01:50 -0400

    getting a response back from ChatGPT API

--- chatbot-code/code.py
+++ chatbot-code/code.py
@@ -12,4 +12,25 @@ def get_completion(prompt, model='gpt-3.5-turbo'):
         messages=messages,
         temperature=0  # this is the degree of randomness of the model's output
     )
-    return response.choices[0].messages["content"]
+
+    return response.choices[0].message["content"]
+
+
+text = f"""
+    Dorothee is sitting in bed and she is my love. I love her so much. \
+    I am really glad that she is here and we can be together! \
+    She is the best! I hope she has a wonderful \
+    and awesome Sunday and a great rest of the week! \
+    """
+
+prompt = f"""
+    Summarize the text delimited by triple backticks into a single sentence.
+
+    Keep the sentence to under ten words.
+    
+    ```{text}```
+    """
+
+response = get_completion(prompt)
+
+print(response)
